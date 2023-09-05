8f61acffa7bc69cb6845d0951860e6343fe4559f
Szymon <szymon@sarnowicz.net>
2023-09-05 02:29:51 -0500

    edit display and loading screen

--- app.js
+++ app.js
@@ -10,9 +10,21 @@ const input = document.querySelector('#entry')
 
 
 async function getMessage() {
-    console.log('clicked')
+    console.log('clicked');
+
+    // work on making this an await for json response
     
-    var textSample = "What can I make with these ingredients? " + input.value
+    for (let i=0; i < 2; i++) {
+        outPutElement.textContent = "\nLOADING";
+        for (let j=0; j < 3; j++) {
+            await delay(1000);
+            outPutElement.append(".")
+        }
+    }
+
+    outPutElement.append("\n\n")
+    
+    var textSample = "What can I make with these ingredients? " + input.value + "Give me your top 3 answers."
     const options = {
         method: 'POST',
         headers: {
@@ -22,21 +34,46 @@ async function getMessage() {
         body: JSON.stringify({
             model: "gpt-3.5-turbo",
             messages: [{role: "user", content: textSample}],
-            max_tokens: 100
+            // max_tokens: 200
         })
     }
     try {
         const response = await fetch('https://api.openai.com/v1/chat/completions', options)
         const data = await response.json()
         console.log(data)
-        outPutElement.textContent = data.choices[0].message.content
+        var food_list = data.choices[0].message.content;
+        outPutElement.textContent = "";
+        var skip_next = false;
+        for (let i=0, len=food_list.length; i < len; i++) {
+
+            is_num = /^[0-9]*$/.test(food_list[i]);
+            following_two_is_num = /^[0-9]*$/.test(food_list[i+1]) || /^[0-9]*$/.test(food_list[i+2]);
+            previous_num = /^[0-9]*$/.test(food_list[i-1]);
+
+            if (is_num && !following_two_is_num && !previous_num && (skip_next == false)) {
+                outPutElement.append("\n");
+                skip_next = true;
+            } else if (!following_two_is_num && !previous_num){
+                skip_next = false;
+            }
+
+            outPutElement.append(food_list[i]);
+        }
+        outPutElement.append("\n\n");
+        // outPutElement.textContent = data.choices[0].message.content
         if (data.choices[0].message.content) {
             const pElement = document.createElement('p')
             pElement.textContent = input.value
         }
-    } catch {
-
+    } catch (error){
+        console.error(error)
     }
+}
+
+function delay(milliseconds){
+    return new Promise(resolve => {
+        setTimeout(resolve, milliseconds);
+    });
 }
 
 function clearInput () {
